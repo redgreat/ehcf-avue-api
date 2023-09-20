84ec90a332a7ee2e17bb601b1b2de96faa0c2e3b
wongcw <rubygreat@msn.com>
2023-09-20 09:40:04 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# fastapi-test
+fastapi测试学习
+
+fork from [fastapi-nano](https://github.com/rednafi/fastapi-nano)
---
+++ app/core/config.py
@@ -0,0 +1,19 @@
+import pathlib
+
+from starlette.config import Config
+
+ROOT = pathlib.Path(__file__).resolve().parent.parent  # app/
+BASE_DIR = ROOT.parent  # ./
+
+config = Config(BASE_DIR / "app/config/zrdb.cnf")
+
+
+API_USERNAME = config("API_USERNAME", str)
+API_PASSWORD = config("API_PASSWORD", str)
+
+# Auth configs.
+API_SECRET_KEY = config("API_SECRET_KEY", str)
+API_ALGORITHM = config("API_ALGORITHM", str)
+API_ACCESS_TOKEN_EXPIRE_MINUTES = config(
+    "API_ACCESS_TOKEN_EXPIRE_MINUTES", int
+)  # infinity
