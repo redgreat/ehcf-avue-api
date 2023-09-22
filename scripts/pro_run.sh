6018562f65ada6053de8985cb1e7c21eb6e339cd
wangcw <rubygreat@msn.com>
2023-09-22 17:04:16 +0800

    修改正式环境端口号

--- scripts/pro_run.sh
+++ scripts/pro_run.sh
@@ -4,4 +4,4 @@
 
 ps aux | grep "gunicorn" | grep -v grep | awk '{print $2}' | xargs kill -9
 
-gunicorn app.main:app --workers 2 --worker-class uvicorn.workers.UvicornWorker -b 0.0.0.0:5001
+gunicorn app.main:app --workers 2 --worker-class uvicorn.workers.UvicornWorker -b 0.0.0.0:9030
