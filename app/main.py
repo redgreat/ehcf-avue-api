c3b32e8a0dcabca22a02b29dc48bebd271792408
wangcw <rubygreat@msn.com>
2023-09-25 09:34:55 +0800

    添加下调试

--- app/main.py
+++ app/main.py
@@ -18,12 +18,12 @@ app.add_middleware(
 # 权限验证
 @app.middleware("http")
 async def verify_app_key(request: Request, call_next):
+    print("开始验证！")
     if request.url.path in ["/docs", "/openapi.json"]:
         response = await call_next(request)
     else:
         app_key = request.headers.get("appKey")
         if app_key != config.APP_KEY or app_key is None:
-            # raise HTTPException(status_code=401, detail="Unauthorized")
             return JSONResponse(status_code=401, content={"detail": "Unauthorized"})
         response = await call_next(request)
     return response
